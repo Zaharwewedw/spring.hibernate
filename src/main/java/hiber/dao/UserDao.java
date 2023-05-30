80a820bc95734d0378025a262cbe25e38a8fddf8
Zahar-Write-Code <tolst.0.2.5.0.2.5@gmail.com>
2023-05-30 20:14:08 +0300

    spring

---
+++ src/main/java/hiber/dao/UserDao.java
@@ -0,0 +1,12 @@
+package hiber.dao;
+
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+   void add(User user);
+   List<User> listUsers();
+   void deleteAllUsers();
+   User findOwner(String car_model, int car_series);
+}
